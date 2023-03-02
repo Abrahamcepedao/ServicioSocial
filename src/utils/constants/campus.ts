f8396e04aca729e88321f709cf1cb2bdbab7925a
Abrahamcepedao <a00827666@itesm.mx>
2023-03-01 19:00:04 -0600

    Added constants

---
+++ src/utils/constants/campus.ts
@@ -0,0 +1,6 @@
+const campus:string[] = [
+    "Monterrey",
+    "Santa Fe"
+]
+
+export default campus
---
+++ src/utils/constants/claves.ts
@@ -0,0 +1,5 @@
+const claves:string[] = [
+    'WA1058', 'WA1067', 'WA1068', 'WA1065', 'WA3041', 'WA1066'
+]
+
+export default claves
