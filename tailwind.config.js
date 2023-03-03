7ea404471816492e77335606a48c7df31da2edf4
Abrahamcepedao <a00827666@itesm.mx>
2023-03-02 18:40:50 -0600

    Updated projects table

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,8 @@ module.exports = {
       'light': '#f9fdff',
       'primary': '#256EFF',
       'secondary': '#d72638',
-      'terciary': '#6c969d'
+      'terciary': '#6c969d',
+      'light-gray': 'rgba(100,100,100,0.15)'
     },
     colors: {
       'black': '#1b1c1d',
