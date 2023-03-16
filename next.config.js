19ec5d6e07dc996fd072c56898043d3d049b1663
Abrahamcepedao <a00827666@itesm.mx>
2023-03-16 00:41:19 -0600

    Configured pwa

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
-const runtimeCaching = require("next-pwa/cache");
 
 const withPWA = require("next-pwa")({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  runtimeCaching,
-  buildExcludes: [/middleware-manifest.json$/]
+  dest:"public",
+  register:true,
+  skipWaiting:true,
+  disable:process.env.NODE_ENV === 'development'
 });
  
 const nextConfig = withPWA({
